4ef8af106f12054f838be30e66ad3b3086a78ea6
TimoSR <timothy.s.rasmussen@gmail.com>
2025-10-12 14:28:54 +0200

    Project Setup

---
+++ Backend/Assignment4/Assignment4/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment4
+{
+    public class DataService
+    {
+    }
+
+    public interface IEntity;
+
+    public interface IAggregateRoot : IEntity;
+
+    public interface IValueObject;
+
+    public interface IRepository;
+
+    public class Order : IAggregateRoot
+    {
+        private Guid ID;
+        private DateOnly Date;
+        
+
+    }
+
+    public class Product : IAggregateRoot
+    {
+
+    }
+}
