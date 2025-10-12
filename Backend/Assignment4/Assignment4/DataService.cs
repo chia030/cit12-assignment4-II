d04727f270e76fd5300991d613a1d3b5fb9048fa
TimoSR <timothy.s.rasmussen@gmail.com>
2025-10-12 14:58:01 +0200

    Building the project

--- Backend/Assignment4/Assignment4/DataService.cs
+++ Backend/Assignment4/Assignment4/DataService.cs
@@ -10,6 +10,25 @@ namespace Assignment4
 {
     public class DataService
     {
+
+        //SOME DTO's
+        
+        public List<Category> GetCategories()
+        {
+            //SOME SQL
+            
+            //MAP THE DATA TO THE CLASS
+        }
+        
+        
+        [Fact]
+        public void GetAllCategories_NoArgument_ReturnsAllCategories()
+        {
+            var service = new DataService();
+            var categories = service.GetCategories();
+            Assert.Equal(8, categories.Count);
+            Assert.Equal("Beverages", categories.First().Name);
+        }
     }
 
     public interface IEntity;
@@ -20,16 +39,43 @@ namespace Assignment4
 
     public interface IRepository;
 
+    // The test wants us to use bad practises
     public class Order : IAggregateRoot
     {
-        private Guid ID;
-        private DateOnly Date;
-        
+        public int Id { get; set; }
+        public DateTime Date {get; set;}
+        public DateTime Required { get; set; }
+        public bool Shipped { get; set; }
+        public OrderDetails OrderDetails { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+    }
 
+    // The test wants us to use bad practises
+    public class OrderDetails : IEntity
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public float UnitPrice { get; set; }
+        public float Quantity { get; set; }
+        public float Discount { get; set; }
     }
 
     public class Product : IAggregateRoot
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float UnitPrice { get; set; }
+        public float QuantityPerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+    }
 
+    public class Category : IAggregateRoot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
