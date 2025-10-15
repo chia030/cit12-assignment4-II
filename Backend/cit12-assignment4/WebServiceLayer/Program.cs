ef53d9ca38ab4a82cf949b2e5f22f4e1a6efbd12
Archit3ct <timothy.s.rasmussen@gmail.com>
2025-10-15 21:57:32 +0200

    Project setup

--- Backend/cit12-assignment4/WebServiceLayer/Program.cs
+++ Backend/cit12-assignment4/WebServiceLayer/Program.cs
@@ -1,18 +1,22 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
@@ -33,6 +37,7 @@ app.MapGet("/weatherforecast", () =>
     })
     .WithName("GetWeatherForecast");
 
+app.UseSwagger();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
